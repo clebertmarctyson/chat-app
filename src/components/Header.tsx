f35754689053a531a18d8df40cecbdfd1baad85e
clebertmarctyson <marctysonclebert9@gmail.com>
2024-07-27 19:54:23 -0400

    New Feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = async () => {
 
   return (
     <header className="flex items-center justify-between p-4 border-b border-border">
-      <h1>OurChat</h1>
+      <h1 className="text-2xl font-bold">OurChat</h1>
 
       <Profile>
         <Avatar className="cursor-pointer">
